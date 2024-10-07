c34ee61ea5911c0cdb6322bbea63da0f2f3aead3
Eric Rosenberg <7120349+ericrosenberg1@users.noreply.github.com>
2024-10-07 13:50:12 -0700

    remove social media keys from this file

--- config-example.py
+++ config-example.py
@@ -6,14 +6,3 @@ OPENSKY_API_PASS = 'your_password'
 
 # Aircraft type configuration
 AIRCRAFT_TYPE = 'Boeing 747'
-
-# Social media API keys and secrets
-SOCIAL_MEDIA_KEYS = {
-    'twitter': {
-        'api_key': 'your_api_key',
-        'api_secret_key': 'your_api_secret_key',
-        'access_token': 'your_access_token',
-        'access_token_secret': 'your_access_token_secret'
-    }
-    # Add other platforms similarly
-}
